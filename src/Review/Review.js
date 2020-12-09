06bd8b644b96f376be47d50f5209bfdce66a0e17
Michael Hicks Richardson <michaelhicksrichardson@Michaels-Mac-mini.local>
2020-12-08 20:46:28 -0500

    reviews are able to be sent

--- src/Review/Review.js
+++ src/Review/Review.js
@@ -1,16 +1,83 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom' 
 import './Review.css'
 
-function Review(){
+class Review extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            review:" "
+        }
+    }
+    reviewChanged(e) {
+        this.setState({
+            review: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const { review } = this.state;
+        const newReview = { review }
+        const url = 'https://mighty-temple-37477.herokuapp.com/api/review'
+        const opt ={
+            method: 'post',
+            body: JSON.stringify(newReview),
+            headers: {
+                'content-Type': 'application/json'
+            }
+        }
+
+        fetch(url,opt)
+        .then(res => {
+            if(!res.ok) {
+                throw new Error('something went wrong please try again')
+            }
+            console.log(res)
+            return res.json()
+        })
+        .then(newReview => {
+            this.setState({
+                newReview:" "
+            })
+            this.props.handleAdd(newReview)
+        })
+        .catch(err => {
+            this.setState({
+                error:err.message
+            })
+            window.location.href='/review-sent'
+        })
+
+    }
+
+    onSubmit(e){
+        e.preventDefault()
+    }
+    render(){
     return(
         <div>
         <h1 class="neon">Review</h1>
     <h2>How was your expreience?</h2>
-    <textarea placeholder="comment"></textarea><br></br>
-    <Link to="Review-Sent" class="req">Send Review</Link>
+    <form
+    onSubmit={e => this.handleSubmit(e)} >
+        <textarea 
+        
+        value={this.state.review}
+        onChange={e => this.reviewChanged(e)}
+        placeholder="comment">
+        </textarea><br></br>
+        {/* <Link to="Review-Sent" class="req">Send Review</Link> */}
+        <button  
+        type='submit'
+        className= "req" >
+            Send Review
+        </button>
+    </form>
     </div>
     )
+    }
 }
 
 export default Review;
