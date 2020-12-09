5b019d6bb85bd6867d49a7803bf8e0f01038f281
Michael Hicks Richardson <michaelhicksrichardson@Michaels-Mac-mini.local>
2020-12-09 14:54:02 -0500

    test started

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import { render } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 
-test('renders learn react link', () => {
+
+test.skip('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it('renders without crashing,',()=>{
+const div = document.createElement('div');
+ReactDOM.render( <BrowserRouter ><App /></BrowserRouter>, div)
+})
